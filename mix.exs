37b7575d6b693b84804e6e7b9433d9b5b233fe15
Mike Ni <michaelcni@gmail.com>
2017-11-07 03:26:07 -0800

    copied files over from phoenix 1.2.5

--- mix.exs
+++ mix.exs
@@ -23,6 +23,7 @@ defmodule PhoenixCustomGenerators.Mixfile do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:phoenix, "> 0.10 and < 1.3.0"}
     ]
   end
 end
