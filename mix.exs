90befec6809aac7642be60ddcd019a2cc7eb3c89
Mike Ni <michaelcni@gmail.com>
2017-11-09 12:59:52 -0800

    copied over from phoenix 1.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PhoenixCustomGenerators.Mixfile do
   def project do
     [
       app: :phoenix_custom_generators,
-      version: "0.0.1",
+      version: "1.0.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       deps: deps()
