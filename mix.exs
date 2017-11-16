763321aa119a31889c269e7a765f17501906a3c0
Mike Ni <michaelcni@gmail.com>
2017-11-16 05:07:27 -0800

    updated mixfile

--- mix.exs
+++ mix.exs
@@ -7,7 +7,10 @@ defmodule PhoenixCustomGenerators.Mixfile do
       version: "1.0.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      deps: deps(),
+      source_url: "https://github.com/mikeni/phoenix_custom_generators",
+      package: package(),
+      description: description(),
     ]
   end
 
@@ -23,7 +26,22 @@ defmodule PhoenixCustomGenerators.Mixfile do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
-      {:ecto, "~> 2.1"} # support for native date, time, datetime types
+      {:ecto, "~> 2.1"}, # support for native date, time, datetime types
+      {:phoenix, "~> 1.3"},
+      {:ex_doc, "~> 0.7", only: :dev},
+    ]
+  end
+
+  defp description() do
+    "Phoenix Generators with ExMachina and JaSerializer Support"
+  end
+
+  defp package() do
+    [
+      # This option is only needed when you don't want to use the OTP application name
+      maintainers: ["Michael Ni"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/mikeni/phoenix_custom_generators"}
     ]
   end
 end
